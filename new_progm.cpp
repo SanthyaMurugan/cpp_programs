181c109c28b040a43e3f9ea5038f64015074b9d4
Santhya <76577092+SanthyaMurugan@users.noreply.github.com>
2021-01-07 12:08:32 +0530

    Tired with GIT

---
+++ new_progm.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"This is the program written for testing purposes"<<endl;
+	cout<<"Thanks and regards\n";
+	cout<<"Check for errors and correct it\n";
+}
---
+++ third_prgm.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"My third programm for testing\n";
+	cout<<"Hoping for the best!"<<endl;
+}
+
+
