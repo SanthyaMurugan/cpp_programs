f03c4206c6945165050ce6c89853a1404b1b2dfe
Santhya <santhya.murugan@cat.com>
2021-01-07 11:37:22 +0530

    added two numbers

--- first_prgm.cpp
+++ first_prgm.cpp
@@ -3,4 +3,8 @@ using namespace std;
 int main()
 {
 	cout<<"My name is Santhya\nThis is the inital phase of the project"<<endl;
+	int a,b;
+	cout<<"Ënter the values of a and b:";
+	cin>>a>>b;
+	cout<<"The sum of "<<a<<" and "<<b<<" is"<<a+b<<endl;
 }
